771e001cd66e7a2de23345d3a51f4decf070c621
csr <esmunred@gmail.com>
2022-11-11 09:10:20 -0500

    solucion4

---
+++ validar.py
@@ -0,0 +1,20 @@
+primerp = print(input('Primer parentecis: '))
+segundop = print(input('Segundo parentecis: '))
+class validez():
+    def __init__(self,par1,par2):
+        par1 == "(" & par2 == ")" | par1 == "[" & par2 == "]" | par1 == "{" & par2 == "}" if print(input('parentesis validos: 😁')) else 'Parentesis no validos 😶'
+
+primer = validez(primerp,segundop)
+
+# segundo
+
+# inicio = int(input('Numero de inicio del sistema: ☠'))
+# fin = int(input('Numero fin del sistema: '))
+
+
+# class sistema():
+#     def __init__(self,num1,num2):
+#         for i in range(num1, num2 + 1):
+#             print(i, end=", ")
+
+# uno = sistema(inicio,fin)
